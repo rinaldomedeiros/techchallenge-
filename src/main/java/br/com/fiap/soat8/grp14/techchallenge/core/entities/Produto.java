75e1f215cf4a69b0e7e11e71feea530c7043d0e3
Matheus Lopes <matheus_santoslopes@hotmail.com>
2024-10-06 10:27:43 -0300

    Correção classes Produto e ProdutoEntity

--- src/main/java/br/com/fiap/soat8/grp14/techchallenge/core/entities/Produto.java
+++ src/main/java/br/com/fiap/soat8/grp14/techchallenge/core/entities/Produto.java
@@ -2,7 +2,15 @@ package br.com.fiap.soat8.grp14.techchallenge.core.entities;
 
 import br.com.fiap.soat8.grp14.techchallenge.app.dto.produto.ProdutoDTO;
 import br.com.fiap.soat8.grp14.techchallenge.core.entities.enums.CategoriaProduto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Produto {
 
     private Long id;
@@ -11,51 +19,16 @@ public class Produto {
     private Double valor;
     private CategoriaProduto categoriaProduto;
 
-    public Produto() {
-    }
-
-    public Produto(Long id, String nome, String descricao, Double valor, CategoriaProduto categoriaProduto) {
-
-        this.id = id;
-        this.nome = nome;
-        this.descricao = descricao;
-        this.valor = valor;
-        this.categoriaProduto = categoriaProduto;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public Double getValor() {
-        return valor;
-    }
-
-    public CategoriaProduto getCategoriaProduto() {
-        return categoriaProduto;
-    }
-
     public Produto(ProdutoDTO produtoDTO) {
         this.id = produtoDTO.getId();
         this.nome = produtoDTO.getNome();
         this.descricao = produtoDTO.getDescricao();
         this.valor = produtoDTO.getValor();
         this.categoriaProduto = produtoDTO.getCategoriaProduto();
-
     }
 
     public ProdutoDTO toProdutoDTO() {
-
         return new ProdutoDTO(this.id, this.nome, this.descricao, this.valor, this.categoriaProduto);
-
     }
 
 }
--- src/main/java/br/com/fiap/soat8/grp14/techchallenge/data/models/ProdutoEntity.java
+++ src/main/java/br/com/fiap/soat8/grp14/techchallenge/data/models/ProdutoEntity.java
@@ -1,20 +1,27 @@
 package br.com.fiap.soat8.grp14.techchallenge.data.models;
 
+import java.io.Serial;
+
 import br.com.fiap.soat8.grp14.techchallenge.core.entities.Produto;
 import br.com.fiap.soat8.grp14.techchallenge.core.entities.enums.CategoriaProduto;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.*;
-
-import java.io.Serial;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@ToString
 @Table(name = "produto")
 @Entity
-
 public class ProdutoEntity extends BaseEntity {
 
     @Serial
@@ -38,19 +45,16 @@ public class ProdutoEntity extends BaseEntity {
     @Column(name = "categoria", nullable = false)
     private CategoriaProduto categoriaProduto;
 
-
     public ProdutoEntity(Produto produto) {
         this.id = produto.getId();
         this.nome = produto.getNome();
         this.descricao = produto.getDescricao();
         this.valor = produto.getValor();
         this.categoriaProduto = produto.getCategoriaProduto();
-
     }
 
     public Produto toProduto() {
         return new Produto(this.id, this.nome, this.descricao, this.valor, this.categoriaProduto);
-
     }
 
 }
