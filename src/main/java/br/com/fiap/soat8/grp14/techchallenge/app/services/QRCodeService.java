1dad2d78f960019bbf9ac1543782f66b576c0507
Rinaldo <Rinaldo@host.docker.internal>
2024-10-12 01:00:01 -0300

    Removendo comentários

--- src/main/java/br/com/fiap/soat8/grp14/techchallenge/app/services/QRCodeService.java
+++ src/main/java/br/com/fiap/soat8/grp14/techchallenge/app/services/QRCodeService.java
@@ -68,53 +68,6 @@ public class QRCodeService {
 		return getQrCode(jsonResponse);
 	}
 
-//    public String gerarQrCode(String nrPedido, List<ItemPedidoDTO> itens, double valorTotal) throws Exception {
-//    	String apiUrl = mercadoPagoConfig.getApiUrl();
-//    	String accessToken = mercadoPagoConfig.getAccessToken();
-//    	String idUsuario = mercadoPagoConfig.getIdUsuario();
-//    	String idPosExterno = mercadoPagoConfig.getIdPosExterno();
-//    	String titulo = mercadoPagoConfig.getTitulo();
-//    	
-//    	StringBuilder urlTemp = new StringBuilder();
-//    	urlTemp.append(apiUrl);
-//    	urlTemp.append("/");
-//    	urlTemp.append(idUsuario);
-//    	urlTemp.append("/pos/");
-//    	urlTemp.append(idPosExterno);
-//    	urlTemp.append("/qrs");
-//    	
-//    	URL url = new URL(urlTemp.toString());
-//    	HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//    	conn.setRequestMethod("POST");
-//    	conn.setRequestProperty("Authorization", "Bearer " + accessToken);
-//    	conn.setRequestProperty("Content-Type", "application/json");
-//    	conn.setDoOutput(true);
-//    	
-//    	String jsonPayload = new JSONObject()
-//    			.put("external_reference", nrPedido)
-//    			.put("title", titulo)
-//    			.put("description", "Pedido " + nrPedido)
-//    			.put("notification_url", "https://www.yourserver.com/notifications") //TODO
-//    			.put("total_amount", valorTotal)
-//    			.put("items", getJsonItens(itens))
-//    			.put("cash_out", new JSONObject().put("amount", 0))
-//    			.toString();
-//    	
-//    	OutputStream os = conn.getOutputStream();
-//    	os.write(jsonPayload.getBytes());
-//    	os.flush();
-//    	
-//    	if (conn.getResponseCode() != 201) {
-//    		throw new RuntimeException("Erro na criação do QR Code: " + conn.getResponseCode());
-//    	}
-//    	
-//    	Scanner scanner = new Scanner(conn.getInputStream(), "UTF-8");
-//    	String jsonResponse = scanner.useDelimiter("\\Z").next();
-//    	scanner.close();
-//    	
-//    	return getQrCode(jsonResponse);
-//    }
-//    
     
     private JSONArray getJsonItens(List<ItemPedidoDTO> itens) {
     	JSONArray jsonItems = new JSONArray();
@@ -142,14 +95,4 @@ public class QRCodeService {
 		return new QRCodeDTO(qrData, orderId);
 	}
 
-//	
-//	private String getQrCode(String jsonResponse) {
-//		JSONObject jsonObject = new JSONObject(jsonResponse);
-//		return jsonObject.getString("qr_data");
-//	}
-//	
-//	private String getIdPedido(String jsonResponse) {
-//		JSONObject jsonObject = new JSONObject(jsonResponse);
-//		return jsonObject.getString("in_store_order_id");
-//	}
 }
